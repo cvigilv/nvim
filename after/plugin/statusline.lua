7e9b7562eb1fc10f6460087eee91e99885de5177
Carlos Vigil Vásquez <cvigil@proterabio.com>
2024-08-15 23:40:07 -0400

    refactor(ui): try new version

--- after/plugin/statusline.lua
+++ after/plugin/statusline.lua
@@ -4,28 +4,28 @@
 
 -- Set custom color groups for tabline
 local setup_statusline_hlgroups = function()
-	local hc = require("user.helpers.colors")
+  local hc = require("user.helpers.colors")
 
-	local colors = {
-		Normal = hc.get_hlgroup_table("TabLineFill"),
-		StatusLine = hc.get_hlgroup_table("StatusLine"),
-		StatusLineNC = hc.get_hlgroup_table("StatusLineNC"),
-	}
+  local colors = {
+    Normal = hc.get_hlgroup_table("TabLineFill"),
+    StatusLine = hc.get_hlgroup_table("StatusLine"),
+    StatusLineNC = hc.get_hlgroup_table("StatusLineNC"),
+  }
 
-	local hlgroups = {
-		StatusLineEmpty = { fg = colors.Normal.fg, bg = colors.Normal.fg },
-		StatusLineCap = { fg = colors.Normal.bg, bg = colors.StatusLine.bg },
-		StatusLineNormal = colors.StatusLineNC,
-		StatusLineImportant = vim.tbl_extend("force", colors.StatusLine, { bold = true }),
-	}
+  local hlgroups = {
+    StatusLineEmpty = { fg = colors.Normal.fg, bg = colors.Normal.bg },
+    StatusLineCap = { fg = colors.Normal.bg, bg = colors.StatusLine.bg },
+    StatusLineNormal = colors.StatusLineNC,
+    StatusLineImportant = vim.tbl_extend("force", colors.StatusLine, { bold = true }),
+  }
 
-	hc.override_hlgroups(hlgroups)
+  hc.override_hlgroups(hlgroups)
 end
 
 vim.api.nvim_create_autocmd("ColorScheme", {
-	group = vim.api.nvim_create_augroup("user::ui", { clear = false }),
-	pattern = "*",
-	callback = setup_statusline_hlgroups,
+  group = vim.api.nvim_create_augroup("user::ui", { clear = false }),
+  pattern = "*",
+  callback = setup_statusline_hlgroups,
 })
 
 -- Setup statusline
@@ -41,48 +41,54 @@ vim.g.qf_disable_statusline = true
 setup_statusline_hlgroups()
 
 _G.statusline = function()
-	local components
-	local is_qf_open = vim.tbl_filter(function(win)
-		return win.quickfix == 1 and win.loclist == 0
-	end, vim.fn.getwininfo())
+  local components = {
+    -- Cap
+    "%#StatusLineEmpty#",
+    ui.align(),
+    ui.leftcap(),
+    -- " ",
 
-	components = {
-		h.ifttt(is_qf_open ~= {}, " ", ui.leftcap()),
-		" ",
-		m.important(h.ifttt(vim.b.gitsigns_status_dict, c.gitbranch(), " " .. c.filepath())),
-		" ",
-		h.ifttt(vim.b.gitsigns_status_dict, c.gitstatus()),
-		" ",
+    -- Modal block
+    m.important(c.mode()),
+    "",
+    h.center(c.location(), 7),
+    "•",
 
-		ui.align(),
-		" ",
-		"%#StatusLineNC#",
-		c.harpoon(),
-		" ",
-		c.fileicon(),
-		h.ifttt(vim.b.gitsigns_status_dict, c.filepath(), nil),
-		m.important(c.filename()),
-		c.filestatus(),
-		" ",
-		c.recording_macro(),
+    -- Buffer block
+    -- " ",
+    c.harpoon(),
+    -- " ",
+    c.fileicon(),
+    h.ifttt(vim.b.gitsigns_status_dict, c.filepath(), nil),
+    m.important(c.filename()),
+    c.filestatus(),
 
-		ui.align(),
-		" ",
-		m.important(c.lsp()),
-		" ",
-		"%#StatusLineNC#",
-		c.diagnostics(),
-		" ",
+    -- Git/CWD block
+    h.ifttt(vim.b.gitsigns_status_dict, "  ", nil),
+    m.important(h.ifttt(vim.b.gitsigns_status_dict, c.gitbranch(), " " .. c.filepath())),
+    -- " ",
+    h.ifttt(vim.b.gitsigns_status_dict, c.gitstatus()),
+    -- " ",
 
-		h.ifttt(is_qf_open ~= {}, " ", ui.rightcap()),
-	}
+    -- Misc block
+    ui.align(),
+    c.recording_macro(),
+    ui.align(),
 
-	return vim.fn.join(
-		vim.tbl_filter(function(value)
-			return value ~= ""
-		end, components),
-		""
-	)
+    -- LSP block
+    -- " ",
+    m.important(c.lsp()),
+    h.ifttt(c.diagnostics() ~= "", "  ", nil),
+    c.diagnostics(),
+    -- " ",
+
+    -- Cap
+    ui.rightcap(),
+    "%#StatusLineEmpty#",
+    ui.align(),
+  }
+
+  return vim.fn.join(vim.tbl_filter(function(value) return value ~= "" end, components), " ")
 end
 
 vim.o.statusline = "%{%v:lua.statusline()%}"
@@ -91,72 +97,59 @@ vim.o.statusline = "%{%v:lua.statusline()%}"
 local augroup = vim.api.nvim_create_augroup("user::statusline", { clear = true })
 -- oil.nvim {{{
 vim.api.nvim_create_autocmd("BufWinEnter", {
-	group = augroup,
-	callback = function()
-		if vim.api.nvim_get_option_value("ft", { scope = "local" }) == "oil" then
-			local components = {
-				ui.leftcap(),
-				ui.align(),
-				" ",
-				c.recording_macro(),
-				m.important(" Oildir"),
-				" ",
-				m.important(c.oil()),
-				c.filestatus(),
-				" ",
-				ui.align(),
-				ui.rightcap(),
-			}
+  group = augroup,
+  callback = function()
+    if vim.api.nvim_get_option_value("ft", { scope = "local" }) == "oil" then
+      local components = {
+        "%#StatusLineEmpty#",
+        ui.align(),
+        ui.leftcap(),
+        " ",
+        c.recording_macro(),
+        m.important(" Oildir"),
+        " ",
+        m.important(c.oil()),
+        c.filestatus(),
+        " ",
+        ui.rightcap(),
+        "%#StatusLineEmpty#",
+        ui.align(),
+      }
 
-			vim.api.nvim_set_option_value(
-				"stl",
-				vim.fn.join(
-					vim.tbl_filter(function(value)
-						return value ~= ""
-					end, components),
-					""
-				),
-				{ scope = "local" }
-			)
-		end
-	end,
+      vim.api.nvim_set_option_value(
+        "stl",
+        vim.fn.join(vim.tbl_filter(function(value) return value ~= "" end, components), ""),
+        { scope = "local" }
+      )
+    end
+  end,
 }) -- }}}
 -- quickfix {{{
 vim.api.nvim_create_autocmd("BufWinEnter", {
-	group = augroup,
-	callback = function()
-		if vim.api.nvim_get_option_value("ft", { scope = "local" }) == "qf" then
-			local components = {
-				ui.leftcap(),
-				" ",
-				c.recording_macro(),
-				m.important("%t%{exists('w:quickfix_title')? ' '.w:quickfix_title : ''}"),
-				" ",
-				ui.align(),
-				ui.rightcap(),
-			}
+  group = augroup,
+  callback = function()
+    if vim.api.nvim_get_option_value("ft", { scope = "local" }) == "qf" then
+      local components = {
+        " ",
+        ui.leftcap(),
+        c.recording_macro(),
+        m.important("%t%{exists('w:quickfix_title')? ' '.w:quickfix_title : ''}"),
+        " ",
+        ui.align(),
+        ui.rightcap(),
+      }
 
-			vim.api.nvim_set_option_value(
-				"stl",
-				vim.fn.join(
-					vim.tbl_filter(function(value)
-						return value ~= ""
-					end, components),
-					""
-				),
-				{ scope = "local" }
-			)
+      vim.api.nvim_set_option_value(
+        "stl",
+        vim.fn.join(vim.tbl_filter(function(value) return value ~= "" end, components), ""),
+        { scope = "local" }
+      )
 
-			vim.api.nvim_set_option_value(
-				"stl",
-				vim.fn.join(
-					vim.tbl_filter(function(value)
-						return value ~= ""
-					end, components),
-					""
-				),
-				{ scope = "local" }
-			)
-		end
-	end,
+      vim.api.nvim_set_option_value(
+        "stl",
+        vim.fn.join(vim.tbl_filter(function(value) return value ~= "" end, components), ""),
+        { scope = "local" }
+      )
+    end
+  end,
 }) -- }}}
--- lua/user/helpers/statusline/components.lua
+++ lua/user/helpers/statusline/components.lua
@@ -4,212 +4,259 @@
 
 M = {}
 
+--- Get current vim mode
+---@return string
+M.mode = function()
+  local mode_lut = { --{{{
+    ["n"] = "NORMAL",
+    ["no"] = "O-PENDING",
+    ["nov"] = "O-PENDING",
+    ["noV"] = "O-PENDING",
+    ["no\22"] = "O-PENDING",
+    ["niI"] = "NORMAL",
+    ["niR"] = "NORMAL",
+    ["niV"] = "NORMAL",
+    ["nt"] = "NORMAL",
+    ["ntT"] = "NORMAL",
+    ["v"] = "VISUAL",
+    ["vs"] = "VISUAL",
+    ["V"] = "V-LINE",
+    ["Vs"] = "V-LINE",
+    ["\22"] = "V-BLOCK",
+    ["\22s"] = "V-BLOCK",
+    ["s"] = "SELECT",
+    ["S"] = "S-LINE",
+    ["\19"] = "S-BLOCK",
+    ["i"] = "INSERT",
+    ["ic"] = "INSERT",
+    ["ix"] = "INSERT",
+    ["R"] = "REPLACE",
+    ["Rc"] = "REPLACE",
+    ["Rx"] = "REPLACE",
+    ["Rv"] = "V-REPLACE",
+    ["Rvc"] = "V-REPLACE",
+    ["Rvx"] = "V-REPLACE",
+    ["c"] = "COMMAND",
+    ["cv"] = "EX",
+    ["ce"] = "EX",
+    ["r"] = "REPLACE",
+    ["rm"] = "MORE",
+    ["r?"] = "CONFIRM",
+    ["!"] = "SHELL",
+    ["t"] = "TERMINAL",
+  } --}}}
+  local mode_code = vim.api.nvim_get_mode().mode
+  if mode_lut[mode_code] == nil then return mode_code end
+
+  return mode_lut[mode_code]
+end
+
 --- Get current path of oil buffer
 ---@return string
 M.oil = function()
-	return "%#StatusLineNormal#"
-		.. vim.fn.expand("%:h:h"):gsub("oil://", "")
-		.. "/"
-		.. "%#StatusLineImportant#"
-		.. vim.fn.expand("%:h:t"):gsub("oil://", "")
-		.. "%#StatusLineNormal#"
+  return "%#StatusLineNormal#"
+    .. vim.fn.expand("%:h:h"):gsub("oil://", "")
+    .. "/"
+    .. "%#StatusLineImportant#"
+    .. vim.fn.expand("%:h:t"):gsub("oil://", "")
+    .. "%#StatusLineNormal#"
 end
 
 --- Get filepath relative to Git repo / CWD
 ---@return string
 M.filepath = function()
-	-- Get relevant paths
-	local cwd = ""
-	if vim.b.gitsigns_status_dict ~= nil then
-		cwd = vim.b.gitsigns_status_dict["root"]
-	else
-		cwd = os.getenv("HOME") or ""
-	end
-	local path = vim.fn.expand("%:p:h"):gsub(cwd, "") .. "/"
+  -- Get relevant paths
+  local cwd = ""
+  if vim.b.gitsigns_status_dict ~= nil then
+    cwd = vim.b.gitsigns_status_dict["root"]
+  else
+    cwd = os.getenv("HOME") or ""
+  end
+  local path = vim.fn.expand("%:p:h"):gsub(cwd, "") .. "/"
 
-	local contents = path
+  local contents = path
 
-	return contents
+  return contents
 end
 
 --- Get current buffer filename
 ---@return string|nil
 M.filename = function()
-	-- Get relevant paths
-	local filename = vim.fn.expand("%:t")
+  -- Get relevant paths
+  local filename = vim.fn.expand("%:t")
 
-	local contents = ""
-	contents = contents .. "%#StatusLineImportant#" .. filename .. "%#StatusLineNormal#"
+  local contents = ""
+  contents = contents .. "%#StatusLineImportant#" .. filename .. "%#StatusLineNormal#"
 
-	return filename
+  return filename
 end
 
 --- Get current git branch of buffer
 ---@return string|nil
 M.gitbranch = function()
-	if vim.b.gitsigns_status_dict ~= nil then
-		return "󰊢 " .. vim.b.gitsigns_head
-	else
-		return nil
-	end
+  if vim.b.gitsigns_status_dict ~= nil then
+    return "󰊢 " .. vim.b.gitsigns_head
+  else
+    return nil
+  end
 end
 
 --- Get current git status of buffer
 ---@return string|nil
 M.gitstatus = function()
-	if vim.b.gitsigns_status_dict ~= nil then
-		-- Add status
-		-- NOTE: Left the highlight groups in case I want to use them in a future
-		local stylesheet = {
-			["added"] = { hl = "DiffAdd", symbol = "+" },
-			["changed"] = { hl = "DiffChange", symbol = "~" },
-			["removed"] = { hl = "DiffDelete", symbol = "-" },
-			["untracked"] = { hl = "DiagnosticWarn", symbol = "!" },
-		}
+  if vim.b.gitsigns_status_dict ~= nil then
+    -- Add status
+    -- NOTE: Left the highlight groups in case I want to use them in a future
+    local stylesheet = {
+      ["added"] = { hl = "DiffAdd", symbol = "+" },
+      ["changed"] = { hl = "DiffChange", symbol = "~" },
+      ["removed"] = { hl = "DiffDelete", symbol = "-" },
+      ["untracked"] = { hl = "DiagnosticWarn", symbol = "!" },
+    }
 
-		local status = {}
-		if vim.b.gitsigns_status_dict["added"] ~= nil then
-			for _, name in ipairs({ "added", "changed", "removed" }) do
-				table.insert(status, stylesheet[name]["symbol"] .. vim.b.gitsigns_status_dict[name])
-			end
-		else
-			local name = "untracked"
-			table.insert(status, "untracked" .. stylesheet[name]["symbol"])
-		end
-		return "(" .. table.concat(status, " ") .. ")"
-	else
-		return nil
-	end
+    local status = {}
+    if vim.b.gitsigns_status_dict["added"] ~= nil then
+      for _, name in ipairs({ "added", "changed", "removed" }) do
+        if vim.b.gitsigns_status_dict[name] > 0 then
+          table.insert(status, stylesheet[name]["symbol"] .. vim.b.gitsigns_status_dict[name])
+        end
+      end
+    else
+      local name = "untracked"
+      table.insert(status, "untracked" .. stylesheet[name]["symbol"])
+    end
+
+    local contents = table.concat(status, " ")
+    if contents ~= "" then
+      return "(" .. table.concat(status, " ") .. ")"
+    else
+      return nil
+    end
+  else
+    return nil
+  end
 end
 
 --- Get attached LSP name of current buffer
 --- TODO: Remove deprecated code
 ---@return string
 M.lsp = function()
-	local msg = "󰌘 "
-	---@diagnostic disable-next-line: deprecated
-	local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-	---@diagnostic disable-next-line: deprecated
-	local clients = vim.lsp.get_active_clients()
+  local msg = "󰌘 "
+  ---@diagnostic disable-next-line: deprecated
+  local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+  ---@diagnostic disable-next-line: deprecated
+  local clients = vim.lsp.get_active_clients()
 
-	-- Return non-"null-ls" servers
-	local buf_clients = {}
-	for _, client in ipairs(clients) do
-		---@diagnostic disable-next-line: undefined-field
-		local filetypes = client.config.filetypes
-		if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-			buf_clients[#buf_clients + 1] = client.name:gsub("_", "-")
-		end
-	end
+  -- Return non-"null-ls" servers
+  local buf_clients = {}
+  for _, client in ipairs(clients) do
+    ---@diagnostic disable-next-line: undefined-field
+    local filetypes = client.config.filetypes
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+      buf_clients[#buf_clients + 1] = client.name:gsub("_", "-")
+    end
+  end
 
-	-- Return complete list of formatters
-	if vim.tbl_count(buf_clients) > 0 then
-		return msg .. vim.fn.join(buf_clients, ",") .. "%#StatusLineNormal#"
-	else
-		return msg .. "No LSP"
-	end
+  -- Return complete list of formatters
+  if vim.tbl_count(buf_clients) > 0 then
+    return msg .. vim.fn.join(buf_clients, ",") .. "%#StatusLineNormal#"
+  else
+    return msg .. "No LSP"
+  end
 end
 
 --- Get diagnostics status of current buffer
 ---@return string
 M.diagnostics = function()
-	local contents = ""
+  local contents = ""
 
-	local severity = { ERROR = 0, WARN = 0, INFO = 0, HINT = 0 }
-	for level, _ in pairs(severity) do
-		severity[level] = vim.tbl_count(vim.diagnostic.get(0, { severity = vim.diagnostic.severity[level] }))
-	end
+  local severity = { ERROR = 0, WARN = 0, INFO = 0, HINT = 0 }
+  local icons = { ERROR = "󰅙 ", WARN = " ", INFO = " ", HINT = " " }
+  for level, _ in pairs(severity) do
+    severity[level] =
+      vim.tbl_count(vim.diagnostic.get(0, { severity = vim.diagnostic.severity[level] }))
+    if severity[level] > 0 then contents = contents .. icons[level] .. severity[level] end
+  end
 
-	contents = "("
-		.. "x"
-		.. severity.ERROR
-		.. " "
-		.. "!"
-		.. severity.WARN
-		.. " "
-		.. "i"
-		.. severity.INFO
-		.. " "
-		.. "?"
-		.. severity.HINT
-		.. ")"
-	return contents
+  return contents
 end
 
 --- Generates a small Harpoon cheat for easier navigation
 ---@return string|nil
 M.harpoon = function()
-	if vim.bo.buftype ~= "" then
-		return
-	end -- not a normal buffer, no harpoon status
+  if vim.bo.buftype ~= "" then return end -- not a normal buffer, no harpoon status
 
-	local ok, harpoon = pcall(require, "harpoon")
-	if not ok then
-		return
-	end -- no harpoon, no harpoon status
+  local ok, harpoon = pcall(require, "harpoon")
+  if not ok then return end -- no harpoon, no harpoon status
 
-	local list = harpoon:list()
-	local contents = "󰛢 "
+  local list = harpoon:list()
+  local contents = "[󰛢 "
 
-	if #list.items == 0 then
-		return nil
-	end -- no items in the list, no harpoon status
+  if #list.items == 0 then return nil end -- no items in the list, no harpoon status
 
-	local current_file = vim.fn.expand("%:p:.")
-	local harpoon_keys = { "h", "j", "k", "l" }
-	if #list.items < 5 then
-		for idx, item in ipairs(list.items) do
-			if item.value == current_file then
-				contents = contents .. "%#StatusLineImportant#" .. harpoon_keys[idx] .. "%#StatusLineNormal#"
-			else
-				contents = contents .. harpoon_keys[idx]
-			end
-		end
-	else
-		for idx = 1, 4, 1 do
-			local item = list.items[idx]
-			if item.value == current_file then
-				contents = contents .. "%#StatusLineImportant#" .. harpoon_keys[idx] .. "%#StatusLineNormal#"
-			else
-				contents = contents .. harpoon_keys[idx]
-			end
-		end
-		contents = contents .. "+"
-	end
+  local current_file = vim.fn.expand("%:p:.")
+  local harpoon_keys = { "h", "j", "k", "l" }
+  if #list.items < 5 then
+    for idx, item in ipairs(list.items) do
+      if item.value == current_file then
+        contents = contents
+          .. "%#StatusLineImportant#"
+          .. harpoon_keys[idx]
+          .. "%#StatusLineNormal#"
+      else
+        contents = contents .. harpoon_keys[idx]
+      end
+    end
+  else
+    for idx = 1, 4, 1 do
+      local item = list.items[idx]
+      if item.value == current_file then
+        contents = contents
+          .. "%#StatusLineImportant#"
+          .. harpoon_keys[idx]
+          .. "%#StatusLineNormal#"
+      else
+        contents = contents .. harpoon_keys[idx]
+      end
+    end
+    contents = contents .. "+"
+  end
 
-	return contents .. ""
+  return contents .. "]"
 end
 
 --- Get file icon of current buffer
 ---@return string
 M.fileicon = function()
-	local filename = vim.fn.expand("%:t")
-	local extension = vim.fn.expand("%:e")
-	local icon, _ = require("nvim-web-devicons").get_icon(filename, extension)
-	if icon ~= nil then
-		return icon .. " "
-	else
-		return ""
-	end
+  local filename = vim.fn.expand("%:t")
+  local extension = vim.fn.expand("%:e")
+  local icon, _ = require("nvim-web-devicons").get_icon(filename, extension)
+  if icon ~= nil then
+    return icon .. " "
+  else
+    return ""
+  end
 end
 
 --- Return vim status of current buffer
 ---@return string Buffer status
-M.filestatus = function()
-	return "%m%r%w%h%q"
-end
+M.filestatus = function() return "%m%r%w%h%q" end
+
+--- Get position of cursor in current buffer
+---@return string Cursor location
+M.location = function() return vim.fn.join(vim.api.nvim_win_get_cursor(0), ",") end
 
 --- Retrieves the current recording macro register.
 -- If no macro is being recorded, returns an empty string.
 -- If a macro is being recorded, returns the register with a symbol prefix.
 -- @return string The current recording macro register with a symbol prefix, or an empty string if no macro is being recorded.
 M.recording_macro = function()
-	local recording_register = vim.fn.reg_recording()
-	if recording_register == "" then
-		return nil
-	else
-		return "󰑋 '" .. recording_register .. "'"
-	end
+  local recording_register = vim.fn.reg_recording()
+  if recording_register == "" then
+    return nil
+  else
+    return "󰑋 '" .. recording_register .. "'"
+  end
 end
 
 return M
