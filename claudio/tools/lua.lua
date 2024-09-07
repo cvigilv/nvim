79fa15fa245a95f5a98b1c5de19df5303669bbd4
Carlos Vigil Vásquez <cvigil@proterabio.com>
2024-09-07 12:32:04 -0400

    fix(lua): change 'mode' to 'action' in docstring config

--- claudio/tools/lua.lua
+++ claudio/tools/lua.lua
@@ -5,7 +5,7 @@ return {
   ["docstring"] = {
     before = h.to_md_codeblock,
     after = h.extract_from_md_codeblock,
-    mode = a.insert_contents,
+    action = a.insert_contents,
     prompt = [[
 You are tasked with generating an EmmyLua docstring for a given Lua function. The function code will be provided to you, and you must return only the docstring. Here is the Lua code for which you need to generate a docstring:
 
