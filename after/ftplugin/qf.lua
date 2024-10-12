44e19cccb5f53245fe95cfe235635ab425c53ba5
Carlos Vigil Vasquez <carlos.vigil.v@gmail.com>
2024-10-12 00:24:03 -0300

    chore: remove unused code and format

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -1,56 +1,31 @@
 -- Load Cfilter(!) command
 vim.cmd.packadd("cfilter")
 
--- TODO: Remove fill chars in quickscope statusline
--- vim.api.nvim_set_option_value("fillchars", {
--- 	eob = " ",
--- 	fold = "·",
--- 	foldopen = "",
--- 	foldsep = "|",
--- 	foldclose = "",
--- }, { scope = "local" })
-
--- Autocommands
-local augroup = vim.api.nvim_create_augroup("user::qf", {})
-
 -- Whenever a quickfix window is opened...
 vim.opt.laststatus = 2
 vim.g.user_qf_open = true
 
 vim.api.nvim_create_autocmd("WinEnter", {
-	group = augroup,
-	desc = "Setup for quickfix window",
-	pattern = "*",
-	callback = function()
-		if vim.o.buftype == "quickfix" then
-			vim.opt.laststatus = 2
-			vim.opt_local.number = false
-			vim.opt_local.relativenumber = false
-			vim.opt_local.fillchars = { -- Characters used for UI things
-				eob = " ",
-				fold = "·",
-				foldopen = "",
-				foldsep = "|",
-				foldclose = "",
-				stl = " ",
-				stlnc = " ",
-			}
-			vim.opt_local.colorcolumn = nil
-
-			-- vim.keymap.set(
-			-- 	"n",
-			-- 	"k",
-			-- 	[[:cprev<CR>:if foldclosed(".") == line(".")<CR>call feedkeys("zo")<CR>endif<CR><C-w>p]],
-			-- 	{ buffer = vim.api.nvim_get_current_buf(), silent = true }
-			-- )
-			-- vim.keymap.set(
-			-- 	"n",
-			-- 	"j",
-			-- 	[[:cnext<CR>:if foldclosed(".") == line(".")<CR>call feedkeys("zo")<CR>endif<CR><C-w>p]],
-			-- 	{ buffer = vim.api.nvim_get_current_buf(), silent = true }
-			-- )
-		end
-	end,
+  group = augroup,
+  desc = "Setup for quickfix window",
+  pattern = "*",
+  callback = function()
+    if vim.o.buftype == "quickfix" then
+      vim.opt.laststatus = 2
+      vim.opt_local.number = false
+      vim.opt_local.relativenumber = false
+      vim.opt_local.fillchars = { -- Characters used for UI things
+        eob = " ",
+        fold = "·",
+        foldopen = "",
+        foldsep = "|",
+        foldclose = "",
+        stl = " ",
+        stlnc = " ",
+      }
+      vim.opt_local.colorcolumn = nil
+    end
+  end,
 })
 
 -- Whenever a quickfix window is closed...
@@ -66,8 +41,8 @@ vim.api.nvim_create_autocmd("QuitPre", {
 
 -- Automatically quit if quickscope window is the last window
 vim.api.nvim_create_autocmd("WinEnter", {
-	group = augroup,
-	pattern = "*",
-	desc = "Automatically quit if quickfix window is the last window",
-	command = [[if winnr('$') == 1 && &buftype == "quickfix"|q|endif]],
+  group = augroup,
+  pattern = "*",
+  desc = "Automatically quit if quickfix window is the last window",
+  command = [[if winnr('$') == 1 && &buftype == "quickfix"|q|endif]],
 })
