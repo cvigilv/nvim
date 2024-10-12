eed75bc228704d05e1e6f9ecd5613e33332bf3b0
Carlos Vigil Vasquez <carlos.vigil.v@gmail.com>
2024-10-12 00:23:15 -0300

    chore(ftplugin): remove useless plugin

--- after/ftplugin/markdown.lua
+++
@@ -1,8 +0,0 @@
-local c = require("patana.palette")
--- Highlight tags
-vim.fn.matchadd("MarkdownTag", "<\\w\\+>\\zs.*\\ze</\\w\\+>")
-vim.fn.matchadd("MarkdownTagName", "<\\w\\+>")
-vim.fn.matchadd("MarkdownTagName", "</\\w\\+>")
-
-vim.cmd("highlight MarkdownTag gui=bold guifg=" .. c.generate_palette().secondary)
-vim.cmd("highlight MarkdownTagName gui=bold guifg=" .. c.generate_palette().accent)
