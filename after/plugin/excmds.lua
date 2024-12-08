08aa67fb9082a824b48c2132fcff987aaec47e7e
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2024-12-08 23:30:39 +0100

    chore(excmds): add comment for sticky shift commands

--- after/plugin/excmds.lua
+++ after/plugin/excmds.lua
@@ -1,3 +1,4 @@
+-- HACK: Sticky shift commands
 vim.api.nvim_create_user_command("Q", ":quit", {})
 vim.api.nvim_create_user_command("Wq", ":wq", {})
 vim.api.nvim_create_user_command("Wqa", ":wqa", {})
