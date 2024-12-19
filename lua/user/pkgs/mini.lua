dd0435d2280410422978c50404d2d3b14956adb7
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2024-12-19 22:03:32 +0100

    refactor(mini): cleanup time

--- lua/user/pkgs/mini.lua
+++ lua/user/pkgs/mini.lua
@@ -1,96 +1,18 @@
 return {
-  { "echasnovski/mini.base16" },
-  { "echasnovski/mini.colors" },
-  {
-    "echasnovski/mini.starter",
-    enabled = false,
-    config = function()
-      local generate_footer = function()
-        local ls = require("lazy").stats()
-        local n_updates = "0"
-        if require("lazy.status").has_updates() then
-          n_updates = require("lazy.status").updates():gsub("🔌", "")
-        end
-        return table.concat({
-          "",
-          "## Relevant keymaps",
-          "",
-          "- ,zc (New Zettelkasten note)",
-          "- ,fz (Search note by title)",
-          "- ,fZ (Search note by content)",
-          "- ,ff (Search Git files)",
-          "- ,fs (Search string using Grep)",
-          "",
-          "",
-          "## Stats",
-          "",
-          "- Loaded "
-            .. ls.loaded
-            .. "/"
-            .. ls.count
-            .. " plugins in "
-            .. ls.startuptime
-            .. " miliseconds",
-          "- " .. n_updates .. " plugins can be updated",
-          -- -- TODO: Implement tasks finder and counter
-          -- "- Tasks found in current directory: " .. "nil",
-          "",
-        }, "\n")
-      end
-
-      require("mini.starter").setup({
-        -- Setup
-        autoopen = true,
-        evaluate_single = true,
-        header = table.concat({
-          -- "# mateo.nvim",
-          -- "",
-          "> mateo: someone who uses his head, smart guy (chilean slang)",
-          "",
-        }, "\n"),
-        items = {
-          -- Add prefix to section header
-          function()
-            local allfiles = require("mini.starter").sections.recent_files(10, true, true)()
-            for _, file in ipairs(allfiles) do
-              file.section = "## " .. file.section
-            end
-            return allfiles
-          end,
-        },
-        footer = generate_footer,
-        query_updaters = [[abcdefghijklmnopqrstuvwxyz0123456789_-.ABCDEFGHIJKLMNOPQRSTUVWXYZ]],
-        content_hooks = {
-          function(content)
-            local blank_content_line = { { type = "empty", string = "" } }
-
-            local section_coords = require("mini.starter").content_coords(content, "section")
-            -- Insert backwards to not affect coordinates
-            for i = #section_coords, 1, -1 do
-              table.insert(content, section_coords[i].line + 1, blank_content_line)
-            end
-
-            return content
-          end,
-          require("mini.starter").gen_hook.adding_bullet("- "),
-          require("mini.starter").gen_hook.aligning("center", "center"),
-        },
-      })
-    end,
-  },
   {
     "echasnovski/mini.ai",
     config = true,
-    -- lazy = "VeryLazy",
+    lazy = "VeryLazy",
   },
   {
     "echasnovski/mini.align",
     config = true,
-    -- keys = { "ga", "gA" },
+    event = "VeryLazy",
   },
 
   {
     "echasnovski/mini.bracketed",
+    event = "VeryLazy",
     config = {
       file = { suffix = "", options = {} },
       indent = { suffix = "", options = {} },
@@ -98,48 +20,10 @@ return {
       treesitter = { suffix = "", options = {} },
       yank = { suffix = "", options = {} },
     },
-    keys = {
-      "[b",
-      "]b",
-      "[B",
-      "]B",
-      "[c",
-      "]c",
-      "[C",
-      "]C",
-      "[x",
-      "]x",
-      "[X",
-      "]X",
-      "[d",
-      "]d",
-      "[D",
-      "]D",
-      "[j",
-      "]j",
-      "[J",
-      "]J",
-      "[l",
-      "]l",
-      "[L",
-      "]L",
-      "[q",
-      "]q",
-      "[Q",
-      "]Q",
-      "[u",
-      "]u",
-      "[U",
-      "]U",
-      "[w",
-      "]w",
-      "[W",
-      "]W",
-    },
   },
   {
     "echasnovski/mini.indentscope",
-    enabled = true,
+    event = "VeryLazy",
     config = function()
       require("mini.indentscope").setup({
         draw = {
@@ -149,59 +33,25 @@ return {
         },
       })
     end,
-    --   require("mini.indentscope").setup({
-    --     draw = {
-    --       delay = 0,
-    --       animation = require("mini.indentscope").gen_animation.none(),
-    --       priority = 2,
-    --     },
-    --     mappings = {
-    --       -- Textobjects
-    --       object_scope = "",
-    --       object_scope_with_border = "",
-    --       goto_top = "",
-    --       goto_bottom = "",
-    --     },
-    --     options = {
-    --       border = "both",
-    --       indent_at_cursor = true,
-    --       try_as_border = false,
-    --     },
-    --
-    --     -- Which character to use for drawing scope indicator
-    --     symbol = "╎",
-    --   })
-    -- end,
-    event = "VeryLazy",
   },
   {
     "echasnovski/mini.comment",
     config = true,
-    keys = {
-      "gcc",
-      "gc",
-    },
+    event = "VeryLazy",
   },
   {
     "echasnovski/mini.surround",
     config = true,
-    keys = {
-      "sa",
-      "sd",
-      "sf",
-      "sF",
-      "sh",
-      "sr",
-      "sn",
-    },
+    event = "VeryLazy",
   },
   {
     "echasnovski/mini.splitjoin",
     config = true,
-    keys = { "gS" },
+    event = "VeryLazy",
   },
   {
     "echasnovski/mini.hipatterns",
+    event = "VeryLazy",
     config = function()
       local hl = require("mini.hipatterns")
       hl.setup({
