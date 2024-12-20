6976a830809d608b3f5265cee573f26d2cfcbd43
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2024-12-20 01:12:14 +0100

    feat(afuera): initial version

--- lua/user/core/keymaps.lua
+++ lua/user/core/keymaps.lua
@@ -3,20 +3,3 @@ local map = vim.keymap.set
 -- Quickfix
 map("n", "]Q", ":cnewer<CR>", { silent = false, desc = "Next quickfix list" })
 map("n", "[Q", ":colder<CR>", { silent = false, desc = "Previous quickfix list" })
-
-vim.g.user_colorcolumn_oob = false
-local function toggle_colorcolumn_oob()
-  if vim.g.user_colorcolumn_oob then
-    vim.cmd("set colorcolumn=96")
-  else
-    vim.cmd("let &colorcolumn=join(range(97,999),',')")
-  end
-  vim.g.user_colorcolumn_oob = not vim.g.user_colorcolumn_oob
-end
-
-map(
-  "n",
-  "<leader><leader>c",
-  toggle_colorcolumn_oob,
-  { silent = true, desc = "Activate colorcolumn OOB view" }
-)
