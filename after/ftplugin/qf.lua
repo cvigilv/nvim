66e85b0731762cf20709c8fa32f208555561a0e4
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2025-01-31 10:19:27 +0100

    chore(ftplugin): remove noicy qf setting

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -1,24 +1,6 @@
 -- Load Cfilter(!) command
 vim.cmd.packadd("cfilter")
 
--- Whenever a quickfix window is opened...
-vim.opt.laststatus = 2
-
-vim.api.nvim_create_autocmd("WinEnter", {
-  desc = "Setup for quickfix window",
-  pattern = "*",
-  callback = function()
-    if vim.o.buftype == "quickfix" then vim.opt.laststatus = 2 end
-  end,
-})
-
--- Whenever a quickfix window is closed...
-vim.api.nvim_create_autocmd("QuitPre", {
-  desc = "Restore setup for quickfix window",
-  callback = function() vim.opt.laststatus = 3 end,
-  once = true,
-})
-
 -- Automatically quit if quickscope window is the last window
 vim.api.nvim_create_autocmd("WinEnter", {
   pattern = "*",
