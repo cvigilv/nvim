dd865a6da1853d36d4dea1825ee2f065b1d8e8c7
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2025-01-31 10:21:13 +0100

    refactor: move to builtin diagnostics

--- after/plugin/diagnostics.lua
+++ after/plugin/diagnostics.lua
@@ -3,40 +3,47 @@
 ---@author Carlos Vigil-Vásquez
 ---@license MIT
 
--- -- Diagnostic symbols for display in the sign column.
--- local symbols = {
---   [vim.diagnostic.severity.ERROR] = "󰅙 ",
---   [vim.diagnostic.severity.WARN] = " ",
---   [vim.diagnostic.severity.HINT] = " ",
---   [vim.diagnostic.severity.INFO] = " ",
--- }
---
--- vim.fn.sign_define("DiagnosticSignError", { text = "󰅙 ", texthl = "DiagnosticSignError" })
--- vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
--- vim.fn.sign_define("DiagnosticSignHint", { text = " ", texthl = "DiagnosticSignHint" })
--- vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
--- vim.fn.sign_define("DiagnosticSignOk", { text = " ", texthl = "Normal" })
---
--- vim.diagnostic.config({
---   update_in_insert = true,
---   severity_sort = true,
---   signs = { text = symbols },
---   virtual_text = {
---     format = function(diagnostic)
---       -- Get line with diagnostics data
---       local line = vim.fn.getline(diagnostic.lnum + 1)
---       line = line:gsub("\t", string.rep(" ", vim.bo.tabstop))
---
---       -- Align diagnostic message with colorcolumn
---       local contents = symbols[diagnostic.severity] .. diagnostic.message
---
---       return "\t\t" .. contents
---     end,
---     prefix = "",
---     spacing = 0,
---     suffix = "",
---   },
--- })
+-- Diagnostic symbols for display in the sign column.
+local symbols = {
+  [vim.diagnostic.severity.ERROR] = "󰅙 ",
+  [vim.diagnostic.severity.WARN] = " ",
+  [vim.diagnostic.severity.HINT] = " ",
+  [vim.diagnostic.severity.INFO] = " ",
+}
+
+vim.fn.sign_define("DiagnosticSignError", { text = "󰅙 ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignHint", { text = " ", texthl = "DiagnosticSignHint" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignOk", { text = " ", texthl = "Normal" })
+
+vim.diagnostic.config({
+  update_in_insert = true,
+  severity_sort = true,
+  signs = { text = symbols },
+  virtual_lines = { current_line = true },
+  virtual_text = {
+    hl_mode = "combine",
+    virt_text_pos = "eol",
+
+    prefix = function(diagnostic, _, total)
+      symbols = {
+        [vim.diagnostic.severity.ERROR] = "E",
+        [vim.diagnostic.severity.WARN] = "W",
+        [vim.diagnostic.severity.INFO] = "I",
+        [vim.diagnostic.severity.HINT] = "H",
+      }
+
+      local severity = diagnostic.severity
+      local symbol = symbols[severity] or "?"
+
+      -- You can customize this format as needed
+      return string.format("%s%d ", symbol, total)
+    end,
+    spacing = 0,
+    suffix = "",
+  },
+})
 
 -- Keymaps
 vim.keymap.set(
