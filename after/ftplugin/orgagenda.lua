fc55a140f22010426f4ff84ba93165c134692f9a
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2025-05-12 09:45:45 +0200

    feat: improve orgagenda view with conceals

--- after/ftplugin/orgagenda.lua
+++ after/ftplugin/orgagenda.lua
@@ -2,4 +2,5 @@ vim.o.textwidth = 96
 
 vim.api.nvim_set_option_value("cc", "", { scope = "local" })
 vim.api.nvim_set_option_value("statuscolumn", "%#Normal#   ", { scope = "local" })
-vim.api.nvim_set_option_value("statusline", "%=%#Bold#orgagenda%=", { scope = "local" })
+vim.api.nvim_set_option_value("conceallevel", 3, {scope = "local"})
+vim.api.nvim_set_option_value("concealcursor", "nvc", {scope = "local"})
