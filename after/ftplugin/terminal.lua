7db95d930e60c314bf8e490943449b4f64cb31d2
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2025-06-18 15:13:25 +0200

    feat(terminal): add some padding

--- after/ftplugin/terminal.lua
+++ after/ftplugin/terminal.lua
@@ -2,7 +2,7 @@
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
 vim.opt_local.signcolumn = "no"
-vim.opt_local.stc = ""
+vim.opt_local.stc = " "
 vim.opt_local.winhighlight = "Normal:OutOfBounds"
 
 -- Keymaps
