a756f862a4c022af12f1182a972a5062c38d2d55
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2025-06-18 15:13:04 +0200

    feat(terminal): set as OOB buffer

--- after/ftplugin/terminal.lua
+++ after/ftplugin/terminal.lua
@@ -3,6 +3,7 @@ vim.opt_local.number = false
 vim.opt_local.relativenumber = false
 vim.opt_local.signcolumn = "no"
 vim.opt_local.stc = ""
+vim.opt_local.winhighlight = "Normal:OutOfBounds"
 
 -- Keymaps
 vim.api.nvim_buf_set_keymap(
