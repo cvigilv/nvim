d5457affe6b356a062761faed459b81a03f8ed60
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2025-09-10 23:56:52 +0200

    chore(pkm): disable contacts plugin

--- after/ftplugin/orgagenda.lua
+++ after/ftplugin/orgagenda.lua
@@ -9,4 +9,4 @@ vim.api.nvim_set_option_value("conceallevel", 3, { scope = "local" })
 vim.api.nvim_set_option_value("concealcursor", "nvc", { scope = "local" })
 
 -- Contacts
-require("plugin.pkm.contacto").setup()
+-- require("plugin.pkm.contacto").setup()
