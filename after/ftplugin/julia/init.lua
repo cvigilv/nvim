d5219dbbbda9b356c4e1f1afd335d15d3bd83a66
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2025-11-21 12:30:08 +0100

    chore: simplify julia ftplugins

--- after/ftplugin/julia/init.lua
+++
@@ -1,7 +0,0 @@
--- Options
-vim.api.nvim_set_option_value("tw", 96, { scope = "local" })
-vim.api.nvim_set_option_value(
-  "formatexpr",
-  "v:lua.require('conform').formatexpr()",
-  { scope = "local" }
-)
