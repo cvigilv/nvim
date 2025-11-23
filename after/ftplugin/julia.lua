27f5a0289f206e00630dc07c1fa4e803e5838edd
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2025-11-23 21:01:00 +0100

    feat: easy folding keys

--- after/ftplugin/julia.lua
+++ after/ftplugin/julia.lua
@@ -4,3 +4,7 @@ vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt_local.foldmethod = "expr"
 vim.opt_local.formatexpr = "v:lua.require('conform').formatexpr()"
 vim.opt_local.textwidth = 96
+
+-- Keymaps
+vim.keymap.set("n", "<Tab>", "za", { noremap = true })
+vim.keymap.set("n", "<S-Tab>", "zA", { noremap = true })
