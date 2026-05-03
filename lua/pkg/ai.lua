29e328390c36eae406173b2f54087dffb400f75b
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2026-05-03 02:05:17 +0200

    refactor: copilot is manually activated with `C-]`

--- lua/pkg/ai.lua
+++ lua/pkg/ai.lua
@@ -1,3 +1,8 @@
+-- Copilot
+vim.g.copilot_filetypes = { ["*"] = false }
+vim.keymap.set("i", "<C-]>", "<Plug>(copilot-suggest)", {desc = "Trigger Copilot suggestion"})
+
+-- CodeCompanion
 require("codecompanion").setup({
   strategies = {
     chat = { adapter = "copilot" },
