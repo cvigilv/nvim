1ea95343f57f96eaeaae4966d3f7cefb704435fe
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2026-09-12 15:41:17 +0200

    chore(typst): dont open pdf associated to file on enter

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -8,10 +8,3 @@ require("plugin.headercolumn").setup(12)
 
 -- Writing mode
 require("plugin.escritura").setup()
-
--- Try to open PDF if exists
-local pdf = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":r") .. ".pdf"
-if vim.loop.fs_stat(pdf) ~= nil then
-  local handle = vim.loop.spawn("sioyek", { args = { pdf }, detached = true }, function() end)
-  if handle then handle:close() end
-end
